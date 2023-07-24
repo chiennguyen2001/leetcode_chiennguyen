5bd021ac31934edce35e696e176063922a22162d
chiennguyen2001 <chienvannguyen2001@gmail.com>
2023-07-24 14:31:34 +0700

    update

---
+++ 42.trapping-rain-water.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=42 lang=python3
+#
+# [42] Trapping Rain Water
+#
+
+# @lc code=start
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        water = 0
+        left_max = height[left]
+        right_max = height[right]
+        while right - left > 0:
+            if left_max < right_max:
+                left += 1
+                left_max = max(left_max, height[left])
+                water += left_max - height[left]
+            else:
+                right -= 1
+                right_max = max(right_max, height[right])
+                water += right_max - height[right]
+        
+        return water        
+# @lc code=end
+
